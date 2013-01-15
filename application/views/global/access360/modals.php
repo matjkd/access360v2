d0bce3a6a7fc8ae282df1f1544575eea53b1df98
Mat Sadler <mat@redstudio.co.uk>
2013-01-15 14:24:40 +0000

    twitter width

--- application/views/global/access360/modals.php
+++ application/views/global/access360/modals.php
@@ -5,7 +5,7 @@
  
   <div class="apple_overlay" id="twitteroverlay">
   	 <div class="twitterWrap">
- <a class="twitter-timeline" href="https://twitter.com/access_360" data-widget-id="291184507749539840">Tweets by @access_360</a>
+ <a class="twitter-timeline" width="640" height="490" href="https://twitter.com/access_360" data-widget-id="291184507749539840">Tweets by @access_360</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 
