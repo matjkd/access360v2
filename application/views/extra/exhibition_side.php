300913823bdf842bc17b00df6f6f6879c1bcabce
Mat Sadler <mat@redstudio.co.uk>
2013-05-15 13:01:52 +0100

    altered exhibitions page

--- application/views/extra/exhibition_side.php
+++ application/views/extra/exhibition_side.php
@@ -1,14 +1,23 @@
 <div class=" padL30 padR20 padT20 padB20">
 	<div class="row-fluid">
-		<div class="bigButton span6"><a href="<?=base_url() ?>custom"><img src="<?=base_url() ?>css/access360/images/CustomButton.jpg"/></a>
+		<div class="span6 bigButton">
+			<a href="<?=base_url() ?>custom"><img src="<?=base_url() ?>css/access360/images/customLogo.png"/>
+				<h3>Custom</h3>
+			</a>
 		</div>
-		<div class="bigButton span6"><a href="<?=base_url() ?>modular"><img src="<?=base_url() ?>css/access360/images/ModularButton.jpg"/></a>
+		<div class="bigButton span6"><a href="<?=base_url() ?>modular"><img src="<?=base_url() ?>css/access360/images/modularLogo.png"/>
+			<h3>Modular</h3>
+		</a>
 		</div>
 	</div>
 	<div class="row-fluid  padT10">
-		<div class="bigButton span6"><a href="<?=base_url() ?>outdoor"><img src="<?=base_url() ?>css/access360/images/OutdoorButton.jpg"/></a>
+		<div class="bigButton span6"><a href="<?=base_url() ?>outdoor"><img src="<?=base_url() ?>css/access360/images/outdoorLogo.png"/>
+			<h3>Outdoor</h3>
+		</a>
 		</div>
-		<div class="bigButton span6"><a href="<?=base_url() ?>roadshow"><img src="<?=base_url() ?>css/access360/images/RoadshowButton.jpg"/></a>
+		<div class="bigButton span6"><a href="<?=base_url() ?>roadshow"><img src="<?=base_url() ?>css/access360/images/roadshowLogo.png"/>
+			<h3>Roadshow</h3>
+		</a>
 		</div>
 
 	</div>
